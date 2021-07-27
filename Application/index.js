a79cbb5fcf8c8492453a65cecb45539ebd75f1f0
Mohammad Faruk <86872019+mdfaruk-github@users.noreply.github.com>
2021-07-27 06:58:18 +0600

    Create index.js

---
+++ Application/index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const { response } = require('express')
+const express = require('express')
+
+// Constants
+const PORT = 8080
+const HOST = '0.0.0.0'
+
+// App
+const app = express()
+
+app.get('/', (request, response) => {
+  response.send('Internal server')
+})
+
+app.get('/api/v1/internal', (request, response) => {
+  response.send('Some data from internal server World')
+})
+app.listen(PORT, HOST)
+console.log(`Internal service running on http://${HOST}:${PORT}`)
+
